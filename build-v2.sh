9bf9343aea540121763e25c2faad105279147ac3
Afshin Karimi <afshin.karimi28@gmail.com>
2024-02-09 23:35:43 +0330

    first commit

--- build-v2.sh
+++ build-v2.sh
@@ -1,3 +1,3 @@
-docker build -t mertcakmak2/spring-argocd-app:v2 .
+docker build -t afshin28/spring-argocd-app:v2 .
 
-docker push mertcakmak2/spring-argocd-app:v2
+docker push afshin28/spring-argocd-app:v2
--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-docker build -t mertcakmak2/spring-argocd-app:v1 .
+docker build -t afshin28/spring-argocd-app:v1 .
 
-docker push mertcakmak2/spring-argocd-app:v1
+docker push afshin28/spring-argocd-app:v1
